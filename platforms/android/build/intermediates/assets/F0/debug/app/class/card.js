21f30129bd4b73647c813a68661d432618195498
N <lotus0602@hanmail.net>
2017-09-02 17:46:46 +0900

    카드놀이, 포커 페이지 분리

---
+++ platforms/android/build/intermediates/assets/F0/debug/app/class/card.js
@@ -0,0 +1,9 @@
+class Card {
+	constructor(number, pattern, color) {
+		this.number = number;
+		this.pattern = pattern;
+		this.color = color;
+	}
+}
+
+exports.Card = Card;
---
+++ platforms/android/src/main/assets/app/solitaire/solitaire-page.js
@@ -0,0 +1,10 @@
+var frameModule = require("ui/frame");
+var SolitaireViewModel = require("./solitaire-view-model");
+
+var solitaireViewModel = new SolitaireViewModel();
+
+function onLoaded(args) {
+
+}
+
+exports.onLoaded = onLoaded;
